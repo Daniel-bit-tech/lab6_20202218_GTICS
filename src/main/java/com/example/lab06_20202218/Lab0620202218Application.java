27030cc8bab9baf0c57a5c49cc738565307e4ee6
daniel_2 <dani6468@gmail.com>
2025-10-02 18:32:05 -0500

    SUBIDA DE PROYECTO

---
+++ src/main/java/com/example/lab06_20202218/Lab0620202218Application.java
@@ -0,0 +1,13 @@
+package com.example.lab06_20202218;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0620202218Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab0620202218Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab06_20202218/Lab0620202218ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab06_20202218;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0620202218ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
